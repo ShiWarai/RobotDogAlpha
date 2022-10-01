ae5623503d9da0ef65212e15bf77e115c8221112
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2022-10-01 17:39:51 +0300

    Update motor.cpp

--- src/model/motor.cpp
+++ src/model/motor.cpp
@@ -4,9 +4,9 @@
 #include <Arduino.h>
 
 float Motor::set_position_by_procent(float proc) {
-    Serial.println("Input...");
+    printf("Input...");
     t_pos = min_pos + proc * abs(max_pos - min_pos);
     Model::need_update[id] = true;
-    Serial.println(t_pos);
-    Serial.println(Model::need_update[id]);
+    printf("%f\r\n", t_pos);
+    printf("%d\r\n", Model::need_update[id]);
 }
