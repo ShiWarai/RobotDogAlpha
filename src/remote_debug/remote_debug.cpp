eab35fadd12773d25f33640cebc0782557fc017f
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2023-03-14 11:40:07 +0300

    Feat: add example to chamge array remote by BLE

--- src/remote_debug/remote_debug.cpp
+++ src/remote_debug/remote_debug.cpp
@@ -1,103 +1,49 @@
 #include "remote_debug.hpp"
 
+#define SERVICE_UUID_OTA              "c8659210-af91-4ad3-a995-a58d6fd26145"
+#define CHARACTERISTIC_UUID_MOTOR0      "c8659212-af91-4ad3-a995-a58d6fd26145"
+
 void RemoteDebug::loop()
 {
-    extern SemaphoreHandle_t model_changed;
+    BLE bt;
 
-    BluetoothSerial* serial_bt = new BluetoothSerial();
-    serial_bt->begin("ESP32 OTA Updates");
+    bt.begin();
 
-    Serial.println("🔁 Remote debug begin");
-
-    String input;
-    while (1) {
-
-        if (serial_bt->available()) { // Если есть данные от Bluetooth
-            input = serial_bt->readString(); // Считываем строку
-            if (input == PIN_CODE) {
-                serial_bt->println("Success"); // Отправляем сообщение об успехе
-                trust_loop(serial_bt);
-            }
-            else { // Иначе
-                serial_bt->println("Wrong pin-code"); // Отправляем сообщение об ошибке
-                serial_bt->disconnect();
-                continue;
-            }
-        }
-        else { // Если нет данных от Bluetooth
-            serial_bt->println("Write pin-code"); // Отправляем запрос на ввод пин-кода
-            vTaskDelay(1000);
-        }
-
+    while(1)
         vTaskDelay(1);
-    }
+}
 
-    delete serial_bt;
+bool BLE::begin() {
+
+    const char* localName = "RDB-1";
+    // Create the BLE Device
+    BLEDevice::init(localName);
+
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new BLECustomServerCallbacks());
+
+    // Create the BLE Service
+    pService = pServer->createService(SERVICE_UUID_OTA);
+
+    pMotorCharacteristic = pService->createCharacteristic(
+                                CHARACTERISTIC_UUID_MOTOR0,
+                                BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+                            );
+
+    pService->start();
+
+    // Start advertising
+    pServer->getAdvertising()->start();
+
+    uint8_t motor0[5] = {0, 0, 0, 0, 0};
+    pMotorCharacteristic->setValue((uint8_t*)motor0, 5);
+
+    return true;
 }
 
 void RemoteDebug::updateModel(SemaphoreHandle_t model_changed) {
     xSemaphoreGive(model_changed);
     vTaskDelay(100);
     xSemaphoreTake(model_changed, portMAX_DELAY);
-}
-
-void RemoteDebug::trust_loop(BluetoothSerial* serial_bt)
-{
-    extern SemaphoreHandle_t model_changed;
-
-    float data[12][3] = {
-        {1.1, 2.2, 3.3},
-        {4.4, 5.5, 6.6},
-        {7.7, 8.8, 9.9},
-        {10.1, 11.2, 12.3},
-        {13.4, 14.5 ,15.6},
-        {16.7 ,17.8 ,18.9},
-        {19.,20.,21,},
-        {22.,23.,24,},
-        {25.,26.,27,},
-        {28.,29.,30,},
-    };
-
-    float pos1, pos2, pos3;
-    float p_pos1, p_pos2, p_pos3;
-    float n_pos1, n_pos2, n_pos3;
-
-    String buf;
-    while(1) {
-        if (serial_bt->available()) {
-            buf = serial_bt->readString();
-
-            switch (buf[0])
-            {
-            case '0':
-            {
-                serial_bt->println(data[0][2]);
-                break;
-            }
-            case 'a':
-            {
-                short id = (buf[1] - 48) * 10 + (buf[2] - 48);
-
-                if (id <= 0 || id > MOTORS_COUNT)
-                {
-                    serial_bt->println("Wrong id!");
-                    break;
-                }
-
-                Model::push_command(Command{ MOTOR_ON, id, 0 });
-                serial_bt->print("Motor on: "); serial_bt->println(id);
-                break;
-            }
-            default:
-                break;
-            }
-
-            xSemaphoreGive(model_changed);
-            vTaskDelay(100);
-            xSemaphoreTake(model_changed, portMAX_DELAY);
-        }
-    }
-
-
-    serial_bt->disconnect();    
 }
--- src/remote_debug/remote_debug.hpp
+++ src/remote_debug/remote_debug.hpp
@@ -21,3 +21,51 @@ private:
     // Bluetooth
     void trust_loop(BluetoothSerial* serial_bt);
 };
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+#include "esp_ota_ops.h"
+
+class BLE; // forward declaration
+
+class BLECustomServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      // deviceConnected = true;
+      // // code here for when device connects
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      // deviceConnected = false;
+      // // code here for when device disconnects
+    }
+};
+
+class otaCallback: public BLECharacteristicCallbacks {
+  public:
+    otaCallback(BLE* ble) {
+      _p_ble = ble;
+    }
+    BLE* _p_ble;
+
+    void onWrite(BLECharacteristic *pCharacteristic);
+};
+
+class BLE
+{
+  public:
+
+    bool begin();
+  
+  private:
+    BLEServer *pServer = NULL;
+
+    BLEService *pESPOTAService = NULL;
+    BLECharacteristic * pESPOTAIdCharacteristic = NULL;
+
+    BLEService *pService = NULL;
+    BLECharacteristic * pMotorCharacteristic = NULL;
+    BLECharacteristic * pOtaCharacteristic = NULL;
+};
+
