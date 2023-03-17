be8c51a19ad85720a000d352a1d6d50fc4d64ada
Shi Warai <zhuravlev.p.r@edu.mirea.ru>
2023-03-17 21:55:59 +0300

    Feat: test service and characteristics

--- src/remote_debug/remote_debug.cpp
+++ src/remote_debug/remote_debug.cpp
@@ -1,7 +1,7 @@
 #include "remote_debug.hpp"
 
-#define SERVICE_UUID_OTA              "c8659210-af91-4ad3-a995-a58d6fd26145"
-#define CHARACTERISTIC_UUID_MOTOR0      "c8659212-af91-4ad3-a995-a58d6fd26145"
+#define SERVICE_UUID                   "e7112e6c-c396-11ed-afa1-0242ac120002"
+#define CHARACTERISTIC_UUID_BEGIN      0xffffff00
 
 void RemoteDebug::loop()
 {
@@ -15,29 +15,73 @@ void RemoteDebug::loop()
 
 bool BLE::begin() {
 
-    const char* localName = "RDB-1";
     // Create the BLE Device
-    BLEDevice::init(localName);
+    BLEDevice::init("RDB-1");
+    BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT);
 
     // Create the BLE Server
     pServer = BLEDevice::createServer();
-    pServer->setCallbacks(new BLECustomServerCallbacks());
+    BLECustomServerCallbacks* callbacks = new BLECustomServerCallbacks(pServer->getAdvertising());
+    pServer->setCallbacks(callbacks);
 
     // Create the BLE Service
-    pService = pServer->createService(SERVICE_UUID_OTA);
+    pService = pServer->createService(BLEUUID(SERVICE_UUID), 1+60*2);
 
-    pMotorCharacteristic = pService->createCharacteristic(
-                                CHARACTERISTIC_UUID_MOTOR0,
-                                BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
-                            );
+    // Security
+    BLESecurity *pSecurity = new BLESecurity();
+    uint8_t rsp_key = ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK;
+    uint8_t auth_option = ESP_BLE_ONLY_ACCEPT_SPECIFIED_AUTH_DISABLE;
+
+    uint32_t passkey = 2284;
+    esp_ble_gap_set_security_param(ESP_BLE_SM_SET_STATIC_PASSKEY, &passkey, sizeof(uint32_t));
+    pSecurity->setAuthenticationMode(ESP_LE_AUTH_REQ_SC_MITM_BOND);
+    pSecurity->setCapability(ESP_IO_CAP_OUT);
+    pSecurity->setKeySize(16);
+    esp_ble_gap_set_security_param(ESP_BLE_SM_ONLY_ACCEPT_SPECIFIED_SEC_AUTH, &auth_option, sizeof(uint8_t));
+    pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
+    esp_ble_gap_set_security_param(ESP_BLE_SM_SET_RSP_KEY, &rsp_key, sizeof(uint8_t));
+
+    float default_value = 0.0;
+    for(int m = 1; m <= 12; m++) {
+        for(int i=0; i < 5; i++) {
+            
+            // Serial.println(CHARACTERISTIC_UUID_BEGIN + 16*m + i);
+            
+            // Create characteristics
+            BLECharacteristic* pMotorCharacteristic = pService->createCharacteristic(
+                                        CHARACTERISTIC_UUID_BEGIN + 16*m + i,
+                                        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+                                    );
+            pMotorCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+
+            switch (i)
+            {
+                case 0:
+                    default_value = Model::motors[m].c_pos;
+                    break;
+                case 1:
+                    default_value = Model::motors[m].kp;
+                    break;
+                case 2:
+                    default_value = Model::motors[m].c_vel;
+                    break;
+                case 3:
+                    default_value = Model::motors[m].kd;
+                    break;
+                case 4:
+                    default_value = Model::motors[m].c_trq;
+                    break;
+                default:
+                    break;
+            }
+            
+            pMotorCharacteristic->setValue(default_value);
+            pService->addCharacteristic(pMotorCharacteristic);
+        }
+    }
 
     pService->start();
-
-    // Start advertising
     pServer->getAdvertising()->start();
-
-    uint8_t motor0[5] = {0, 0, 0, 0, 0};
-    pMotorCharacteristic->setValue((uint8_t*)motor0, 5);
 
     return true;
 }
--- src/remote_debug/remote_debug.hpp
+++ src/remote_debug/remote_debug.hpp
@@ -5,67 +5,54 @@
 #include "freertos/semphr.h"
 #include "../model/command.hpp"
 #include "../model/model.hpp"
-#include "BluetoothSerial.h"
-
-#define PIN_CODE "2284"
-
-class RemoteDebug
-{
-public:
-    RemoteDebug() {};
-    void loop();
-private:
-    // Misc
-    void updateModel(SemaphoreHandle_t model_changed);
-
-    // Bluetooth
-    void trust_loop(BluetoothSerial* serial_bt);
-};
-
 #include <BLEDevice.h>
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
 #include "esp_ota_ops.h"
 
-class BLE; // forward declaration
+class RemoteDebug
+{
+public:
+	RemoteDebug(){};
+	void loop();
 
-class BLECustomServerCallbacks: public BLEServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      // deviceConnected = true;
-      // // code here for when device connects
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      // deviceConnected = false;
-      // // code here for when device disconnects
-    }
+private:
+	// Misc
+	void updateModel(SemaphoreHandle_t model_changed);
 };
 
-class otaCallback: public BLECharacteristicCallbacks {
-  public:
-    otaCallback(BLE* ble) {
-      _p_ble = ble;
-    }
-    BLE* _p_ble;
 
-    void onWrite(BLECharacteristic *pCharacteristic);
+class BLECustomServerCallbacks : public BLEServerCallbacks
+{
+public:
+
+	BLECustomServerCallbacks(BLEAdvertising* advertising) {
+		this->advertising = advertising;
+	}
+
+private:
+	BLEAdvertising* advertising;
+
+	void onConnect(BLEServer *pServer)
+	{
+		// Serial.println("Connect");
+		return;
+	};
+
+	void onDisconnect(BLEServer *pServer)
+	{
+		this->advertising->start();
+		// Serial.println("Disconnect");
+		return;
+	}
 };
 
 class BLE
 {
-  public:
-
-    bool begin();
-  
-  private:
-    BLEServer *pServer = NULL;
-
-    BLEService *pESPOTAService = NULL;
-    BLECharacteristic * pESPOTAIdCharacteristic = NULL;
-
-    BLEService *pService = NULL;
-    BLECharacteristic * pMotorCharacteristic = NULL;
-    BLECharacteristic * pOtaCharacteristic = NULL;
+public:
+	bool begin();
+private:
+	BLEServer *pServer = NULL;
+	BLEService *pService = NULL;
 };
-
